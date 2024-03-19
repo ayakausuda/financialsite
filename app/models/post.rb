e7fed7997200938fdb300ae3a7aa9176061a9736
Ayaka Usuda <usuda.ayaka@gmail.com>
2024-03-19 20:01:40 +0900

    3/19ー１

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,6 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments, dependent: :destroy
 
-    mount_uploader :image, ImageUploader
-
   #tweetsテーブルから中間テーブルに対する関連付け
   has_many :post_tag_relations, dependent: :destroy
   #tweetsテーブルから中間テーブルを介してTagsテーブルへの関連付け
--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,13 @@ Rails.application.routes.draw do
   get 'perfumes/credit' => 'perfumes#credit'
   get 'perfumes/nisa' => 'perfumes#nisa'
 
-  resources :posts do
-    resources :comments, only: [:create]
-  end
-
   resources :perfumes
-
 
   get 'posts/top' => 'posts#top'
   root 'posts#top'
+
+  resources :posts do
+    resources :comments, only: [:create]
+  end
 
 end
